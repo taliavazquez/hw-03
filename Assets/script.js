3cb7b08699b26bc1f2f4deb3fba97c195b522a3e
taliavazquez <vazqueztalia@outlook.com>
2020-07-20 22:52:36 -0700

    updates JS to prompt user, need to create the generator next

--- Assets/script.js
+++ Assets/script.js
@@ -1,45 +1,51 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate"); 
+var generateBtn = document.querySelector('#generate');
 
+//function - To use a function, you must define it somewhere in the scope from which you wish to call it
 function writePassword() {
+   //variables hold data value and it can be changed anytime
   var password = generatePassword();
-  var passwordText = document.querySelector(“#password”);
+  //create a password to the #password input
+  var passwordText = document.querySelector('#password');
   passwordText.value = password;
 }
 
+//function - To use a function, you must define it somewhere in the scope from which you wish to call it
+function generatePassword(){
+   //The first dialog box you see after pressing "Generate Password"
+  var length = prompt('Choose a password length of at least 8 characters and no more than 128 characters\.');
+     //user enters number and now the program runs an if statement
+    if (length < 8 || length > 128) {
+    //dialog box with alert    
+    alert('Length does not meet the requirement\.');
+
+    return;
+  }
+  //The second dialog box you see after pressing "Generate Password" if you answer within 8-128
+  var lowercase = confirm('Do you want lowercase letters in your password?');
+  //The third dialog box you see after pressing "Generate Password" if you answer within 8-128
+  var uppercase = confirm('Do you want uppercase letters in you password?');
+  //The fourth dialog box you see after pressing "Generate Password" if you answer within 8-128
+  var specialCharacter = confirm('Would you like to include special characters?');
+  //The fifth dialog box you see after pressing "Generate Password" if you answer within 8-128
+  var numericValue = confirm('Would you like numbers in you password?')
+
+//set possible password values
+
+//Need generator function with loop to choose password characters
+
+//double check that user selected all requirements
+
+//Make sure characters are added to the password
 
 
-
-function generatePassword() {
-  var length = prompt(‘choose a length of at least 8 characters and no more than 128 characters’);
-  if (length < 8 || length > 128) {
-    alert(‘Length does not meet the requirement.’);
-    return;
-  }
-  var lowerCase = confirm(‘Do you want lowercase in your password ?’);
-  if () {
-    alert(‘’);
-    return;
-  }
-  var upperCase = confirm(‘Do you want uppercase in you password ?’);
-  if () {
-    alert(‘’);
-    return;
-  }
-  var specialCharacter = confirm(‘Would you like to include special characters ?’);
-  if () {
-    alert(‘’);
-    return;
-  }
-  var numericValue = confirm(‘Would you like numbers in you password ?’)
-  if () {
-    alert(‘’);
-    return;
-  }
   var pw = createPW()
   return pw;
 }
 
 
+function createPW(){
+  console.log("uppercase");
+}
 
-generateBtn.addEventListener(“click”, writePassword);
+
+generateBtn.addEventListener("click", writePassword);
